c52233a602a87af8b1e78926a15e91ffb1dd7b9e
MBlazina <mirko.blazina@gmail.com>
2020-03-17 00:39:33 +0100

    first commit

---
+++ src/routes/_layout.svelte
@@ -0,0 +1,15 @@
+<script>
+	import Nav from '../components/Nav.svelte';
+
+	export let segment;
+</script>
+
+<style lang="scss" global>
+ @import "./style/global.scss";
+</style>
+
+<Nav {segment}/>
+
+<main class="lg:mt-20 container mx-auto p-4">
+	<slot></slot>
+</main>
---
+++ src/routes/index.svelte
@@ -0,0 +1,20 @@
+<style lang="scss">
+	h1{
+		color: coral;
+		&:hover{
+			color: darkolivegreen;
+		}
+	}
+</style>
+<svelte:head>
+	<title>Sapper project template</title>
+</svelte:head>
+
+<h1 class="text-center text-2xl my-4">Great success!</h1>
+
+<div class="flex flex-col items-center mb-4 bg-blue-200">
+	<img alt='Sapper Logo' class="w-2/3 lg:w-1/3" src='sapper-logo.svg'>
+	<img alt='Tailwind Logo' class="w-2/3 lg:w-1/3" src='tailwind-logo.svg'>
+</div>
+
+<p class="text-center"><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
