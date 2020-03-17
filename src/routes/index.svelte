25bf9a8c54df9a03f33e16f0d3a7fb6e45ecb18e
MBlazina <mirko.blazina@gmail.com>
2020-03-17 03:27:42 +0100

    comm

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -5,6 +5,9 @@
 			color: darkolivegreen;
 		}
 	}
+	h6{
+		color:pink;
+	}
 </style>
 <svelte:head>
 	<title>Sapper project template</title>
@@ -16,5 +19,4 @@
 	<img alt='Sapper Logo' class="w-2/3 lg:w-1/3" src='sapper-logo.svg'>
 	<img alt='Tailwind Logo' class="w-2/3 lg:w-1/3" src='tailwind-logo.svg'>
 </div>
-
 <p class="text-center"><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
